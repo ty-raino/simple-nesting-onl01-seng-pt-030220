ecc036d5af6c42e9f5303a9c54785f03703ff4a8
Ty Raino <mstymetris@yahoo.com>
2020-03-15 18:30:15 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -20,8 +20,6 @@ programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
-	programmer_hash[:known_for]
-
 	programmer_hash =
  		{
         :grace_hopper => {
@@ -37,6 +35,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+		 programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
