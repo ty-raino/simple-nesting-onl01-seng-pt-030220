3112086ee90a6212343e72bd63cf441403f40872
Ty Raino <mstymetris@yahoo.com>
2020-03-15 19:13:35 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -79,7 +79,15 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+		 # add the following information to the top level of programmer_hash
+		 # :yukihiro_matsumoto => {
+		 #   :known_for => "Ruby",
+		 #    :languages => ["LISP", "C"]
+		 # }
+		 programmer_hash[:yukihiro_matsumoto] = {
+			 known_for: "Ruby",
+			 languages: "LISP", "C"
+		 }
 
 end
 
