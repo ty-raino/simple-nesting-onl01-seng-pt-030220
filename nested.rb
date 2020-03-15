7cea78751ed932488b5d7d7897e7053805a14949
Ty Raino <mstymetris@yahoo.com>
2020-03-15 18:27:39 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -22,7 +22,7 @@ end
 def alan_kay_is_known_for
 	programmer_hash[:alan_kay][:known_for]
 
-	programmer_hash = 
+	programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
